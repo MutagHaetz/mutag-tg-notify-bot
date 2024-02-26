52943419e6bbe69b2025edc64f4a52a49334415f
GoodPointt <dev6012@gmail.com>
2024-02-26 12:10:44 +0200

    feat: translate notify messages on he

--- src/notify/notify.service.ts
+++ src/notify/notify.service.ts
@@ -5,56 +5,53 @@ import { NotificationDto } from './dto/notification.dto';
 
 @Injectable()
 export class NotifyService {
-	constructor(private readonly botUpdate: BotUpdate) { }
+	constructor(private readonly botUpdate: BotUpdate) {}
 
 	async notifyMessage(dto: NotificationDto) {
-		let message: string
+		let message: string;
 
 		if (!dto.orderPrice) {
 			message =
-				`🆘Received new contact request📞:
+				// `🆘Received new contact request📞:
+				`🆘 קיבלתי בקשת קשר חדשה 📞:
 
-			Name: ${dto.name}
-			Email: ${dto.email}
-			Phone: ${dto.phone}`
+שם: ${dto.name}
+דואר: ${dto.email}
+טלפון: ${dto.phone}`;
 		}
 
-			if (dto.orderPrice) {
-				let goodsList = ''
-				if (dto.goods.length > 0) {
-					goodsList = dto.goods.map(({ title, count }) => `${title}: x${count}, `).join('')
-				}
-			
-				message =
-					`✅Received new order🛒:
-			OrderNum: ${dto.orderNum}
-
-			FirstName: ${dto.firstName}
-			LastName: ${dto.lastName}
-
-			Email: ${dto.email}
-			Phone: ${dto.phone}
-
-			Delivery: ${dto.delivery}
-			OrderPrice: ${dto.orderPrice}
-			Goods: ${goodsList}`;
-		}
-		
-			return await this.botUpdate.sendMessage(message);
-		}
-
-	
-
-	async checkNotify(): Promise < string > {
-			try {
-				console.log(process.env.HOST_URL)
-			const response = await axios.get(process.env.HOST_URL + '/notify');
-				return response.data;
-			} catch(error) {
-				console.error('Error when checking notify:', error);
-				throw error;
+		if (dto.orderPrice) {
+			let goodsList = '';
+			if (dto.goods.length > 0) {
+				goodsList = dto.goods
+					.map(({ title, count }) => `${title}: x${count}, `)
+					.join('');
 			}
+
+			// message = `✅Received new order🛒:
+			message = `✅ התקבלה הזמנה חדשה 🛒:
+
+מספר הזמנה: ${dto.orderNum}
+שם: ${dto.firstName}
+שם משפחה: ${dto.lastName}
+דואר: ${dto.email}
+טלפון: ${dto.phone}
+הובלה ל: ${dto.delivery}
+מחיר הזמנה: ${dto.orderPrice}
+מוצרים: ${goodsList}`;
 		}
+
+		return await this.botUpdate.sendMessage(message);
 	}
 
-
+	async checkNotify(): Promise<string> {
+		try {
+			console.log(process.env.HOST_URL);
+			const response = await axios.get(process.env.HOST_URL + '/notify');
+			return response.data;
+		} catch (error) {
+			console.error('Error when checking notify:', error);
+			throw error;
+		}
+	}
+}
